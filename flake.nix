fbd3fb44536b835bfd6b8e2a31b9d51823096851
utopiaeh <utopiaeh01@gmail.com>
2026-02-07 18:50:38 +0200

    upd

--- flake.nix
+++ flake.nix
@@ -4,23 +4,28 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, fenix }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
 
         rustToolchain = fenix.packages.${system}.fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-REPLACE_ME";
+          # ✅ Precomputed working hash for ESP32-S3 + components
+          sha256 = "1dwbjzvcnk6fzr1vwz3kh26j9kp27s0c1f0pm1vxy0zj5fbdx5km";
         };
-      in {
+      in
+      {
         devShells.default = pkgs.mkShell {
           packages = [
             rustToolchain
@@ -28,5 +33,6 @@
             pkgs.ldproxy
           ];
         };
-      });
+      }
+    );
 }
